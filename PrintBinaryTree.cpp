d3fd04f01c7f64e95cfcfff57228afbb0ad4f1f9
Charlene Jiang <charlene@tsynka.com>
2012-10-12 10:32:53 +0800

    Add print Binary Tree

---
+++ PrintBinaryTree.cpp
@@ -0,0 +1,75 @@
+/*
+
+   Print the Binary Tree:
+   
+             A 
+            / \
+            B C
+           /  / \
+           D  E F
+            /  \
+            G  H
+            
+            A
+            B C
+            D E F
+            G H
+*/            
+             
+typedef Node {
+  int data;
+  Node *left;
+  Node *right;
+}
+
+void searchAndPrint(int currentLine, int line, Node* root)
+{
+  if (line == currentLine)
+  {
+    cout << root->data << ' ';
+  }
+  
+  if (root == NULL)
+  {
+    return;
+  }
+  
+  if (root->left) 
+  {
+    searchAndPrint(currentLine + 1, line, root->left);
+  }
+  
+  if (root->right)
+  {
+    searchAndPrint(currentLine + 1, line, root->right);
+  }
+}
+
+int searchAndGetLines(Node* root, int currentLine)
+{
+  if (root == NULL)
+  {
+    return currentLine;
+  }
+  
+  int leftCount = searchAndGetLines(root->left, currentLine + 1);
+  int rightCount = searchAndGetLines(root->right, currentLine + 1);
+  
+  return leftCount > rightCount ? leftCount : rightCount;
+}
+
+int linesOfTree(Node root)
+{
+  return searchAndGetLines(&root, 0);
+} 
+
+void printTree(Node root)
+{
+  int count = linesOfTree(root);
+  
+  for (int i = 0; i < count; i ++) 
+  {
+    searchAndPrint(0, i, &root);
+    cout << endl;
+  }
+}
